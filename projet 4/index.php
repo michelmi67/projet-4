0827aea123e844490a85ae00530754d7663c6e83
Michel Lechevrel <michel.lechevrel@gmail.com>
2020-10-14 14:41:42 +0200

    essais commit

--- projet 4/index.php
+++ projet 4/index.php
@@ -65,7 +65,8 @@
                     <span id='lire'><a href="&&&">lire la suite</a></span>
             </div> 
         </div>
-
+            <p>essais commit</p>
+            
         <script src="main.js"></script>
     </body>
 </html>
