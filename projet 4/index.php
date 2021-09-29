eafc0885b7854d8823c2cae28d98e6376ff4ce85
michelmi67 <michel.lechevrel@gmail.com>
2021-09-29 11:46:34 +0200

    ajout d'une route article

--- projet 4/index.php
+++ projet 4/index.php
@@ -2,13 +2,20 @@
 require('controller/controller.php');
 
 if (isset($_GET['action'])) {
+    //URL = monsite.fr?action=accueil
     if ($_GET['action'] == 'accueil') {
         accueil();
     }
+    //URL = monsite.fr?action=index_article
     elseif ($_GET['action'] == 'index_article') {
         index_articles();
     }
-}
+    //URL = monsite.fr?action=artcile&texte=$id
+    elseif($_GET['action'] == 'article') {
+        $id = $_GET['texte'];
+        article($id);
+    }
+}    
 else {
     accueil();
 }
