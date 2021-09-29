9ea371b24c0ca84dfcc721ac6eb1f0146883f364
michelmi67 <michel.lechevrel@gmail.com>
2021-09-29 11:47:52 +0200

    ajout fonction article

--- projet 4/controller/controller.php
+++ projet 4/controller/controller.php
@@ -14,3 +14,10 @@ function index_articles()
     $all_articles = recup_all_articles();
     require('views/index_article.php');
 }
+
+function article($id)
+{
+    $article = recup_article($id);
+    $commentaires = recup_commentaires($id);
+    require('views/article.php');
+}
